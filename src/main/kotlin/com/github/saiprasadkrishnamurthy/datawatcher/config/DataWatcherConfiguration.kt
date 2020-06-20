ba72386ffb68531eee65f389b094b6b310bc7738
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2020-06-20 15:02:42 +0530

    Refactor

--- src/main/kotlin/com/github/saiprasadkrishnamurthy/datawatcher/config/DataWatcherConfiguration.kt
+++ src/main/kotlin/com/github/saiprasadkrishnamurthy/datawatcher/config/DataWatcherConfiguration.kt
@@ -1,12 +1,16 @@
 package com.github.saiprasadkrishnamurthy.datawatcher.config
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.github.saiprasadkrishnamurthy.datawatcher.model.PolicyDefinition
-import com.github.saiprasadkrishnamurthy.datawatcher.repository.PolicyDefinitionRepository
-import org.springframework.boot.CommandLineRunner
+import com.google.common.base.Predicates
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.nio.file.Paths
+import org.springframework.core.env.Environment
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 import java.util.concurrent.ForkJoinPool
 
 @Configuration
@@ -15,10 +19,22 @@ class DataWatcherConfiguration {
     @Bean("workerPool")
     fun workerPool(): ForkJoinPool = ForkJoinPool(Runtime.getRuntime().availableProcessors(), ForkJoinPool.defaultForkJoinWorkerThreadFactory, null, true)
 
-   /* @Bean
-    fun command(policyDefinitionRepository: PolicyDefinitionRepository) = CommandLineRunner {
-        val mapper = jacksonObjectMapper()
-        val p = mapper.readValue(Paths.get("policy.json").toFile(), PolicyDefinition::class.java)
-        policyDefinitionRepository.save(p)
-    }*/
+    @Bean
+    fun configApi(environment: Environment): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .groupName("data-watcher-service")
+                .apiInfo(apiInfo(environment))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error"))) // Exclude Spring error controllers
+                .build()
+    }
+
+    private fun apiInfo(environment: Environment): ApiInfo {
+        return ApiInfoBuilder()
+                .title("Data Watcher Service | REST APIs")
+                .contact(Contact("Saiprasad Krishnamurthy", "https://github.com/SaiprasadKrishnamurthy", "saiprasad.krishnamurthy@gmail.com"))
+                .version("Build: " + environment.getProperty("build.version")!!)
+                .build()
+    }
 }
